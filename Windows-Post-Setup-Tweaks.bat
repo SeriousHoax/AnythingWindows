94cbbc56e4a554223b8ced24a49cbbb7bedc0a97
SeriousHoax <serioushoax@gmail.com>
2022-10-23 21:15:20 +0600

    Update Windows-Post-Setup-Tweaks.bat

--- Windows-Post-Setup-Tweaks.bat
+++ Windows-Post-Setup-Tweaks.bat
@@ -247,6 +247,9 @@ sc config DiagTrack start= disabled
 rem Remote Desktop Services
 sc config TermService start= disabled
 
+rem Windows Remote Management (WS-Management)
+sc config WinRM start= disabled
+
 rem =================================== Windows Settings ===================================
 rem ------------------------------------ Accessibility ------------------------------------
 rem ...................................... Keyboard .......................................
