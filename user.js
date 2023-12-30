be13e840a5a7ddd4ddd8a46cb91ebe3f275e1da7
SeriousHoax <serioushoax@gmail.com>
2023-12-30 23:02:34 +0600

    Update user.js

--- user.js
+++ user.js
@@ -1,36 +1,42 @@
 /*
 *    Name: SeriousHoax's user.js for Firefox
-*    Date: 29 November 2023
-* Version: 16
+*    Date: 30 December 2023
+* Version: 17
 *     URL: https://github.com/SeriousHoax/AnythingWindows/blob/main/user.js
 */
 
 user_pref("accessibility.force_disabled", 1);
 user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.compactmode.show", true);
 user_pref("browser.contentblocking.category", "custom");
 user_pref("browser.download.dir", "E:\\Downloads\\Browser Downloads");
 user_pref("browser.download.folderList", 2);
 user_pref("browser.tabs.firefox-view", false);
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("browser.newtab.privateAllowed", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
 user_pref("browser.newtabpage.activity-stream.showSearch", false);
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
-user_pref("browser.newtabpage.activity-stream.topSitesRows", 2);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.topSitesRows", 0);
 user_pref("browser.privateWindowSeparation.enabled", false);
 user_pref("browser.promo.focus.enabled", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("browser.tabs.loadBookmarksInBackground", true);
 user_pref("browser.toolbars.bookmarks.visibility", "always");
 user_pref("browser.uidensity", 1);
+user_pref("browser.urlbar.showSearchTerms.enabled", false);
 user_pref("cookiebanners.service.mode", 2);
 user_pref("cookiebanners.service.mode.privateBrowsing", 2);
 user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.webextensions.restrictedDomains", "");
 user_pref("layout.css.prefers-color-scheme.content-override", 0);
@@ -41,6 +47,7 @@ user_pref("network.cookie.cookieBehavior", 5);
 user_pref("network.dns.echconfig.enabled", true);
 user_pref("network.http.referer.disallowCrossSiteRelaxingDefault", true);
 user_pref("network.http.referer.disallowCrossSiteRelaxingDefault.top_navigation", true);
+user_pref("network.http.windows-sso.enabled", true);
 user_pref("network.IDN_show_punycode", true);
 user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.fingerprintingProtection", true);
@@ -50,6 +57,7 @@ user_pref("privacy.query_stripping.enabled", true);
 user_pref("privacy.query_stripping.enabled.pbmode", true);
 user_pref("privacy.trackingprotection.cryptomining.enabled", true);
 user_pref("privacy.trackingprotection.emailtracking.enabled", true);
+user_pref("privacy.trackingprotection.enabled", false);
 user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
 user_pref("privacy.trackingprotection.pbmode.enabled", false);
 user_pref("signon.rememberSignons", false);
