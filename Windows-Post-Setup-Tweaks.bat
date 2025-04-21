c3ad4d9d46d62e187c1245933effa28678d4710d
Serious Hoax <serioushoax@gmail.com>
2025-04-21 21:07:57 +0600

    Update Firewall Rules

--- Windows-Post-Setup-Tweaks.bat
+++ Windows-Post-Setup-Tweaks.bat
@@ -612,20 +612,21 @@ reg add "HKCU\Control Panel\Cursors" /v "Person" /t reg_EXPAND_SZ /d "%%SystemRo
 
 rem Windows Firewall Block
 
-netsh advfirewall firewall add rule name="AdGuardHome" dir=in action=allow program="C:\Program Files\AdGuardHome\AdGuardHome.exe" enable=yes description="AdGuardHome"
-netsh advfirewall firewall add rule name="CCleaner" dir=out action=block program="E:\Portables\Ccleaner Portable\CCleaner64.exe" enable=yes description="CCleaner"
-netsh advfirewall firewall add rule name="Google Chrome TestingBox" dir=in action=block program="E:\Portables\Chrome Portable TestingBox\Chrome\Chrome.exe" enable=yes description="Google Chrome"
-netsh advfirewall firewall add rule name="Google Chrome Report" dir=in action=block program="E:\Portables\Chrome Portable Report\Chrome\Chrome.exe" enable=yes description="Google Chrome"
-netsh advfirewall firewall add rule name="Microsoft Compatibility Telemetry" dir=out action=block program="C:\Windows\System32\CompatTelRunner.exe" enable=yes description="Microsoft Compatibility Telemetry"
-netsh advfirewall firewall add rule name="deluge" dir=in action=block program="C:\Program Files\Deluge\deluge.exe" enable=yes description="deluge"
-netsh advfirewall firewall add rule name="EagleGet Free Downloader" dir=in action=block program="E:\Portables\EagleGet Protable\EagleGet.exe" enable=yes description="EagleGet Free Downloader"
-netsh advfirewall firewall add rule name="filec" dir=in action=block program="E:\Portables\File Centipede\filec.exe" enable=yes description="filec"
-netsh advfirewall firewall add rule name="fileu" dir=in action=block program="E:\Portables\File Centipede\fileu.exe" enable=yes description="fileu"
-netsh advfirewall firewall add rule name="Firefox" dir=in action=block program="C:\Program Files\Mozilla Firefox\firefox.exe" enable=yes description="Firefox"
-netsh advfirewall firewall add rule name="LocalSend" dir=in action=block program="E:\Portables\LocalSend\localsend_app.exe" enable=yes description="LocalSend"
-netsh advfirewall firewall add rule name="Microsoft Edge" dir=in action=block program="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" enable=yes description="Microsoft Edge"
-netsh advfirewall firewall add rule name="qBittorrent - A Bittorrent Client" dir=in action=block program="E:\Portables\qBittorrent Portable\App\qBittorrent\qbittorrent.exe" enable=yes description="qBittorrent - A Bittorrent Client"
-netsh advfirewall firewall add rule name="WOMicClient" dir=in action=block program="C:\Program Files (x86)\WOMic\WOMicClient.exe" enable=yes description="WOMicClient"
+netsh advfirewall firewall add rule name="AdGuardHome" dir=in action=allow program="C:\Program Files\AdGuardHome\AdGuardHome.exe" enable=yes
+netsh advfirewall firewall add rule name="CCleaner" dir=out action=block program="E:\Portables\Ccleaner Portable\CCleaner64.exe" enable=yes
+netsh advfirewall firewall add rule name="Google Chrome TestingBox" dir=in action=block program="E:\Portables\Chrome Portable TestingBox\Chrome\Chrome.exe" enable=yes 
+netsh advfirewall firewall add rule name="Google Chrome Report" dir=in action=block program="E:\Portables\Chrome Portable Report\Chrome\Chrome.exe" enable=yes
+netsh advfirewall firewall add rule name="Microsoft Compatibility Telemetry" dir=out action=block program="C:\Windows\System32\CompatTelRunner.exe" enable=yes
+netsh advfirewall firewall add rule name="deluge" dir=in action=block program="C:\Program Files\Deluge\deluge.exe" enable=yes
+netsh advfirewall firewall add rule name="EagleGet Free Downloader" dir=in action=block program="E:\Portables\EagleGet Protable\EagleGet.exe" enable=yes
+netsh advfirewall firewall add rule name="filec" dir=in action=block program="E:\Portables\File Centipede\filec.exe" enable=yes
+netsh advfirewall firewall add rule name="fileu" dir=in action=block program="E:\Portables\File Centipede\fileu.exe" enable=yes
+netsh advfirewall firewall add rule name="Firefox" dir=in action=block program="C:\Program Files\Mozilla Firefox\firefox.exe" enable=yes
+netsh advfirewall firewall add rule name="LocalSend" dir=in action=block program="E:\Portables\LocalSend\localsend_app.exe" enable=yes
+netsh advfirewall firewall add rule name="Microsoft Edge" dir=in action=block program="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" enable=yes description
+netsh advfirewall firewall add rule name="qBittorrent Portable" dir=in action=block program="E:\Portables\qBittorrent Portable\qbittorrent.exe" enable=yes
+netsh advfirewall firewall add rule name="Tixati Portable" dir=in action=block program="E:\Portables\Tixati Portable\tixati_Windows64bit.exe" enable=yes
+netsh advfirewall firewall add rule name="WOMicClient" dir=in action=block program="C:\Program Files (x86)\WOMic\WOMicClient.exe" enable=yes
 
 rem Microsoft Defender Hash Log, Startup Update, Cloud Protection Level, Cloud Timeout Extend and PUP Protection
 
