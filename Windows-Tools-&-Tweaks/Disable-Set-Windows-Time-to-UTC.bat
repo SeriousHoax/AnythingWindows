5a7c0283e88b36dd99a68c8f0969e21413fa3233
Serious Hoax <serioushoax@gmail.com>
2025-07-30 03:18:49 +0600

    Update UTC time bat files

--- Windows-Tools-&-Tweaks/Disable-Set-Windows-Time-to-UTC.bat
+++ Windows-Tools-&-Tweaks/Disable-Set-Windows-Time-to-UTC.bat
@@ -1,11 +1,11 @@
 @echo off
-REM Remove RealTimeIsUniversal from the registry
+rem Remove RealTimeIsUniversal from the registry
 echo Removing RealTimeIsUniversal from the registry...
 reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /f
 
-REM Set w32time service to demand start
-echo Setting w32time service to demand start...
-sc config w32time start= demand
+rem Set w32time service to Automatic (Delayed start)
+echo Enabling the w32time service...
+sc config w32time start= delayed-auto
 
 echo Done.
 pause
--- Windows-Tools-&-Tweaks/Set_Windows_Time_to_UTC.bat
+++ Windows-Tools-&-Tweaks/Set_Windows_Time_to_UTC.bat
@@ -1,11 +1,21 @@
 @echo off
-REM Apply registry settings to set RealTimeIsUniversal to 1
+rem Apply registry settings to set RealTimeIsUniversal to 1
 echo Applying registry settings...
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /t REG_DWORD /d 1 /f
 
-REM Disable the w32time service
+rem Disable the w32time service
 echo Disabling w32time service...
 sc config w32time start= disabled
 
-echo Done.
+rem Windows's built-in time syncing method is often buggy and doesn't work. So the "Update Time" app by Sordum has been used - https://www.sordum.org/9203/update-time-v1-3/
+rem copy UpdateTime folder to Program Files - Copying to the program files is not necessary but recommended to store it in the system drive
+echo copying updatetime folder to program files...
+xcopy "E:\Portables\UpdateTime" "C:\Program Files\UpdateTime\" /e /i /y
+
+rem create the Update-Time service to sync time at every system startup
+echo creating Update-Time service...
+sc create Update-Time binpath= "\"C:\Program Files\UpdateTime\UpdateTime.exe\" /Service" displayname= "Update Time v1-3" start= auto
+sc description Update-Time "Automatically Synchronize Computer date and time on Windows Startup."
+
+echo done.
 pause
