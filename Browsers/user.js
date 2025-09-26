20766a59bd240fd9f0fed2337e6493b977868e73
SeriousHoax <serioushoax@gmail.com>
2025-09-26 23:22:56 +0600

    Update user.js

--- Browsers/user.js
+++ Browsers/user.js
@@ -1,7 +1,7 @@
 /*
 *    Name: SeriousHoax's user.js for Firefox
-*    Date: 23 January 2024
-* Version: 21
+*    Date: 2025-09-26
+* Version: 22
 *     URL: https://github.com/SeriousHoax/AnythingWindows/blob/main/user.js
 */
 
@@ -34,6 +34,7 @@ user_pref("browser.toolbars.bookmarks.visibility", "always");
 user_pref("browser.uidensity", 1);
 user_pref("browser.urlbar.autoFill.adaptiveHistory.enabled", true);
 user_pref("browser.urlbar.showSearchTerms.enabled", false);
+user_pref("browser.urlbar.trimURLs", false);
 user_pref("cookiebanners.service.mode", 2);
 user_pref("cookiebanners.service.mode.privateBrowsing", 2);
 user_pref("datareporting.healthreport.uploadEnabled", false);
